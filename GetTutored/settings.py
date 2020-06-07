2f55620860294e29cc807ad6dabcc78536e9ccf0
Dickson Olumide Sosanya <sosanyaolumidedoubled@gmail.com>
2020-06-08 00:08:12 +0100

    Integrated Django with React(Reactified Django)

--- GetTutored/settings.py
+++ GetTutored/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'frontend',
     'tutors',
     'parents',
     'rest_framework',
--- GetTutored/urls.py
+++ GetTutored/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
+    path('', include('frontend.urls')),
     path('admin/', admin.site.urls),
     path('tutors_api/', include('tutors.urls')),
     path('parents_api/', include('parents.urls')),
