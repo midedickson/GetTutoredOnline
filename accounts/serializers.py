db5df2e6665a7b64529dc27e33a1be2c49ba6bff
Dickson Olumide Sosanya <sosanyaolumidedoubled@gmail.com>
2020-06-10 02:58:28 +0100

    Added Unique E-mail Upon Registration

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -21,13 +21,19 @@ class RegisterSerilizer(serializers.ModelSerializer):
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
+        email = self.validated_data['email']
+        email_qs = User.objects.filter(email=email)
+        if email_qs.exists():
+            raise serializers.ValidationError(
+                'This e-mail has already been used by another user'
+            )
+
         user = User.objects.create_user(
             validated_data['username'], validated_data['email'], password=validated_data['password'])
         user.first_name = self.validated_data['first_name']
         user.last_name = self.validated_data['last_name']
         user.save()
         return user
-
 # Login Serializer
 
 
