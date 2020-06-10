6890a23e19d007c1b05faf9cf42766e0b0787d9a
Dickson Olumide Sosanya <sosanyaolumidedoubled@gmail.com>
2020-06-10 03:50:45 +0100

    Started Working on Dynamic Filter

--- tutors/views.py
+++ tutors/views.py
@@ -2,19 +2,56 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import generics, permissions
+from django.db.models import Q, Count
 
-from .models import Tutor
+from .models import Tutor, Subject
 from .serializers import TutorSerializer
 from .permissions import IsOwnerOrReadOnly
+
+
+def is_valid_queryparam(param):
+    return param != '' and param is not None
+
+
+def filter(request):
+    qs = Tutro.objects.all()
+    # Parameters to be sent to the backend
+    name_contains_query = request.GET.get('name_contains')
+    price_min = request.GET.get('price_min')
+    price_max = request.GET.get('price_max')
+    subject = request.GET.get('subject')
+    grade = request.GET.get('grade')
+
+    if is_valid_queryparam(name_contains_query):
+        qs = qs.filter(
+            Q(info__first_name__icontains=name_contains_query) |
+            Q(info__last_name__icontains=name_contains_query)
+        ).distinct()
+
+    if is_valid_queryparam(price_min):
+        qs = qs.filter(price__gte=price_min)
+
+    if is_valid_queryparam(price_max):
+        qs = qs.filter(price__lt=price_max)
+
+    if is_valid_queryparam(expertise) and expertise != 'Choose...':
+        qs = qs.filter(expertise__name=subject)
+
+    if is_valid_queryparam(grade) and grade != 'Choose...':
+        qs = qs.filter(expertise__grade=grade)
+
+    return qs
 
 
 class TutorList(generics.ListAPIView):
     """
     List all tutors.
     """
-
-    queryset = Tutor.objects.all()
     serializer_class = TutorSerializer
+
+    def get_queryset(self):
+        qs = filter(self.request)
+        return qs
 
 
 class TutorCreate(generics.CreateAPIView):
@@ -53,3 +90,33 @@ class TutorDetail(generics.RetrieveUpdateDestroyAPIView):
 
     queryset = Tutor.objects.all()
     serializer_class = TutorSerializer
+
+
+"""
+def infinite_filter(request):
+    limit = request.GET.get('limit')
+    offset = request.GET.get('offset')
+    return Journal.objects.all()[int(offset): int(offset) + int(limit)]
+
+
+def is_there_more_data(request):
+    offset = request.GET.get('offset')
+    if int(offset) > Journal.objects.all().count():
+        return False
+    return True
+
+class ReactInfiniteView(generics.ListAPIView):
+    serializer_class = JournalSerializer
+
+    def get_queryset(self):
+        qs = infinite_filter(self.request)
+        return qs
+
+    def list(self, request):
+        queryset = self.get_queryset()
+        serializer = self.serializer_class(queryset, many=True)
+        return Response({
+            "journals": serializer.data,
+            "has_more": is_there_more_data(request)
+        })
+"""
